23da35f934b90a4520e56072cd616aa9cc75bafa
Daniel Moura <daniel-moura@outlook.com>
2023-01-27 17:33:27 -0300

    feat: create theme variables

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -9,4 +9,27 @@ export const {
   getCssText,
   theme,
   createTheme
-} = createStitches()
+} = createStitches({
+  theme: {
+    colors: {
+      black: '#000',
+      white: '#FFF',
+
+      gray100: '#F2F2F2',
+      gray300: '#A6A6A6',
+      gray500: '#717171',
+      gray700: '#3E3E3E',
+
+      pink: '#FBC0BF',
+      
+      coral: '#FB7673'
+    },
+
+    fontSizes: {
+      sm: '0.75 rem',
+      md: '1 rem',
+      lg: '1.35 rem',
+      xl: '2 rem'
+    }
+  }
+})
