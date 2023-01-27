d7956f2bc2bd915a5f1b35479a852772515ec1cd
Daniel Moura <daniel-moura@outlook.com>
2023-01-27 17:38:05 -0300

    feat: add google font lato and init a header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,24 @@
 import type { AppProps } from 'next/app'
+import { Lato } from '@next/font/google'
+import React from 'react'
+import { globalStyles } from '../styles/global'
+
+const lato = Lato({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets:['latin']
+})
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={lato.className}>
+      <header>
+        <h1>Cake Store</h1>
+      </header>
+
+      <Component {...pageProps} />
+    </main>
+  )
 }
