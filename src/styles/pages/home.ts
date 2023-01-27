eb28ea14d4553bfa80bc1a15803eb7cbd11eceec
Daniel Moura <daniel-moura@outlook.com>
2023-01-27 17:33:55 -0300

    feat: init style home

---
+++ src/styles/pages/home.ts
@@ -0,0 +1,12 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main', {
+  display: 'flex',
+  width: '100%',
+  maxWidth: '80%',
+  margin: '0 auto'
+});
+
+export const Product = styled('a', {
+
+})
